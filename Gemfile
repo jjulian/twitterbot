2562b828336662788cd477140db0a4dc26b777ee
Jonathan Julian <jonathan.m.julian@gmail.com>
2013-04-15 17:07:11 -0400

    add tests

--- Gemfile
+++ Gemfile
@@ -1,6 +1,9 @@
-source :rubygems
+source 'https://rubygems.org'
 
 gem 'twitter', '>= 4.0.0'
 gem 'hashie'
 gem 'sanitize'
 gem 'hpricot'
+
+# development
+gem 'mocha', require: false
---
+++ twitterbot_test.rb
@@ -0,0 +1,23 @@
+require 'test/unit'
+require 'mocha/setup'
+require 'twitterbot'
+
+class TwitterbotTest < Test::Unit::TestCase
+  def setup
+    Twitter.stubs(:update)
+  end
+  def test_returns_the_tweet
+    text = "My snarky comment."
+    result = Twitterbot.tweet { text }
+    assert result == text, "should return the tweet text"
+  end
+  def test_trims_text_at_140_chars
+    text = 'X' * 200
+    result = Twitterbot.tweet { text }
+    assert result.size == 140
+  end
+  def test_fails_if_error
+    Twitter.stubs(:update).raises(Twitter::Error::RateLimited)
+    assert_raises(Twitter::Error::RateLimited) { Twitterbot.tweet(sleep_time: 0.01) { "My thoughts." } }
+  end
+end
